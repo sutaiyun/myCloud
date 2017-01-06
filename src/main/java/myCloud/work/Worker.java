2b9476596df91add0eb594bdd1125891f7c38d23
sutaiyun <sutaiyun@longcheer.net>
2017-01-06 21:24:05 +0800

    调整 三个 编译代码目录。  三个子项目代码互相隔离。

--- src/main/java/myCloud/work/Worker.java
+++ src/main/java/myCloud/work/Worker.java
@@ -6,5 +6,14 @@ package myCloud.work;
 public class Worker {
     public static void main(String args[]) {
         System.out.print("This is Worker main!!!!!!");
+        System.out.println("");
+        System.out.println("****************************************");
+        System.out.println("**                                    **");
+        System.out.println("**                                    **");
+        System.out.println("**               Note:                **");
+        System.out.println("**         Shutdown: cmd: exit!       **");
+        System.out.println("**                                    **");
+        System.out.println("****************************************");
+        System.out.println("");
     }
 }
